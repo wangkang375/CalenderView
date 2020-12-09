31ca20c3db9c5c6a0db19c154da3072d249eba2a
wk <420220193@qq.com()>
2020-12-09 15:15:42 +0800

    toast

--- app/src/main/java/com/moreecare/calendarapplication/MainActivity.kt
+++ app/src/main/java/com/moreecare/calendarapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.moreecare.calendarapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.moreecare.calendarlibrary.calendar.CalendarView
 import com.moreecare.calendarlibrary.calendar.DayCBean
 import com.moreecare.calendarlibrary.calendar.SelectDayListener
@@ -16,6 +17,11 @@ class MainActivity : AppCompatActivity(), SelectDayListener {
     }
 
     override fun onSelectDayListener(dayCBean: DayCBean) {
+        Toast.makeText(
+            this,
+            "${dayCBean.year}年${dayCBean.month + 1}月${dayCBean.day}日",
+            Toast.LENGTH_SHORT
+        ).show()
         Log.d("dayCBean", "dayCBean:${dayCBean.year}年${dayCBean.month + 1}月${dayCBean.day}日")
     }
 }
